9f5662961e3d22d08f1d9bc24f5dce55a9a68cab
KAFE6060 <165509707+KAFE6060@users.noreply.github.com>
2024-04-09 18:01:21 +0100

    Add files via upload

---
+++ PyramidAnimation/pyramidNoOOP.cpp
@@ -0,0 +1,63 @@
+#include "cubeNoOOP.h"
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <SDL.h>
+
+struct Point2D { float x; float y; };
+struct Point3D { float x; float y; float z; };
+struct Edge { int start; int end; };
+
+int WindowSizeX;
+int WindowSizeY;
+SDL_Renderer* renderer;
+float DeltaTime = 0;
+float FL = 5;
+
+
+std::vector<Point3D> points2{
+         Point3D{ -1.8, -1.8, -1.8 }, Point3D{ -1.8, -1.8, 1.8 },
+         Point3D{ 1.8,  -1.8, -1.8 }, Point3D{ -1.8,  1.8, -1.8 },
+         Point3D{ -1.8, 1.8, 1.8 }, Point3D{ 1.8,  -1.8, 1.8 },
+         Point3D{ 1.8,  1.8, -1.8 }, Point3D{ 1.8,  1.8, 1.8 }
+};
+
+
+std::vector<Edge> edges{
+         Edge{ 0, 1 },
+         Edge{ 1, 3 },
+         Edge{ 3, 2 },
+         Edge{ 2, 0 },
+         Edge{ 0, 4 },
+         Edge{ 1, 4 },
+         Edge{ 2, 4 },
+         Edge{ 3, 4 },
+
+};
+*/
+
+Point2D projection2(Point3D point)
+{
+    return Point2D{ WindowSizeX / 2 + (point.x * FL) / (FL + point.z) * 100, WindowSizeY / 2 + (point.y * FL) / (FL + point.z) * 100 };
+}
+
+void render2(std::vector<Point3D> points, std::vector<Edge> edges)
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+
+    //rotation += 1 * DeltaTime;
+
+    for (auto& edge : edges) {
+        Point2D start = projection2(points[edge.start]);
+        Point2D end = projection2(points[edge.end]);
+
+        SDL_RenderDrawLine(renderer, start.x, start.y, end.x, end.y);
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
+
+
